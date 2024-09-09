da2552b49cfd9c89281a262863737980fa8de4c4
j <77560326+Jxl-s@users.noreply.github.com>
2024-09-09 01:02:18 -0400

    add nginx

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
   await app.listen(5001);
 }
