853bd2d25c71e183c997501f673e63b453e356c7
j <77560326+Jxl-s@users.noreply.github.com>
2024-09-09 00:28:47 -0400

    setup backend docker development config

--- backend/src/auth/strategies/google.strategy.ts
+++ backend/src/auth/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
-      scope: ['email', 'profile'],
+      scope: ['email'],
     });
   }
 
