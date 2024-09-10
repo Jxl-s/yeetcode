9d66851cbd87afb2274c8118123e159e8ae58903
j <77560326+Jxl-s@users.noreply.github.com>
2024-09-09 23:54:01 -0400

    setup shadcn

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+	import LogOut from 'lucide-svelte/icons/log-out';
+	import IDCard from 'lucide-svelte/icons/id-card';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import { AuthState, authStore, axiosInstance, signOut } from '$lib/stores/auth';
 
 	$: signedIn = $authStore.state === AuthState.SignedIn;
@@ -6,25 +9,33 @@
 </script>
 
 <h1 class="text-3xl font-bold">Welcome to YeetCode</h1>
-<ul>
+<p>A clone of a certain popular code execution platform.</p>
+<ul class="mt-2 flex gap-2">
 	{#if signedOut}
 		<li>
-			<a href="/api/auth/google">Sign In</a>
+			<Button>
+				<LogOut class="mr-2 h-4 w-4" />
+				<a href="/api/auth/google">Sign In</a>
+			</Button>
 		</li>
 	{/if}
 	{#if signedIn}
 		<li>
-			<button on:click={signOut}>Sign Out</button>
+			<Button variant="destructive" on:click={signOut}>
+				<LogOut class="mr-2 h-4 w-4" />
+				Sign Out
+			</Button>
 		</li>
 		<li>
-			<button
+			<Button
+				variant="secondary"
 				on:click={async () => {
 					const res = await axiosInstance.get('/auth/protected');
 					console.log('res', res);
 				}}
 			>
 				Try protected
-			</button>
+			</Button>
 		</li>
 	{/if}
 </ul>
